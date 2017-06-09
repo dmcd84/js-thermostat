54bbecde9e0433a9ac236c3784c3343667e3b986
Dom <dom.mcdonnell@gmail.com>
2017-06-09 10:16:56 +0100

    update app to set include get request on rackup

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,12 @@
 require 'sinatra/base'
 require 'json'
+require 'sinatra/cross_origin'
 
 class Thermostat < Sinatra::Base
+
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+end
 
 get '/' do
   'You is well cool!'
